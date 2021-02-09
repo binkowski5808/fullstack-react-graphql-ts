3b718b89d9607000d63d2bbfdefc30357124611b
Mariusz Bińkowski <binkowski5808@portal.ron.mil.pl>
2021-02-09 22:36:24 +0100

    Set up redis and express session to create cookie and keep session for user

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
+if (!__prod__) require("dotenv").config();
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -15,12 +21,33 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        sameSite: "lax",
+        httpOnly: true,
+        secure: __prod__, //only in prod (localhost is not working in https)
+      },
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
